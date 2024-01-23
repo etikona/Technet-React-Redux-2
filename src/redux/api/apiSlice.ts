11c7c7e6cdd56c3feff706d7a7bddce3cbed5a82
eti kona <etikonapal@gmail.com>
2024-01-23 21:06:16 +0600

    redux mutation implemented

--- src/redux/api/apiSlice.ts
+++ src/redux/api/apiSlice.ts
@@ -10,7 +10,16 @@ export const api = createApi({
     singleProduct: builder.query({
       query: (id) => `/product/${id}`,
     }),
+    // Mutation
+    postComment: builder.mutation({
+     query : ({id, data}) => {
+      url : `/comment/${id}`,
+      method: 'POST',
+      body: data,
+
+     }
+    }),
   }),
 });
 
-export const { useGetProductsQuery, useSingleProductQuery } = api;
+export const { useGetProductsQuery, useSingleProductQuery, usePostCommentMutation } = api;
